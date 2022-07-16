5b4a56ea32d682d6b35cc9a3be5a6f0aee431641
DaiTangoMne <mksim3uchnev@gmail.com>
2022-07-16 10:39:58 +0300

    added: -function single number edit -greetings

--- main.py
+++ main.py
@@ -5,54 +5,56 @@ from bs4 import BeautifulSoup
 
 oppers = ["мегафон", "tele2", "мтс", "билайн"]
 
+ua = UserAgent()
 
-def get_opp(file_name):
+http_proxy = "http://195.135.242.141:8081"
+
+proxies = {
+    'http': http_proxy,
+    # 'https': https_proxy,
+}
+
+headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;'
+              'q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'user-agent': ua.random,
+}
+
+
+def file_edit(file_name):
     with open(file_name, 'r', encoding='utf-8') as file:
         li = [item for item in file.read().split('\n')]
 
     ready_numbers = []
 
-    ua = UserAgent()
-
-    http_proxy = "http://195.135.242.141:8081"
-
-    proxies = {
-        'http': http_proxy,
-        # 'https': https_proxy,
-    }
-
-    headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;'
-                  'q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'user-agent': ua.random,
-    }
-
     for phone_number in li:
-        data = {
-            'number': f'{phone_number}',
-        }
-
-        try:
-            response = requests.post("https://www.kody.su/check-tel", headers=headers,
-                                     data=data, proxies=proxies)
-            html = response.text
-
-            soup = BeautifulSoup(html, "lxml")
-            content_in = soup.find("div", class_="content__in").text.lower()
-            for opp in oppers:
-                if opp in content_in:
-                    ready_numbers.append(f'{phone_number} - {opp.capitalize()}')
-                    break
-            else:
-                ready_numbers.append(f'{phone_number} - Другие')
-        except Exception as ex:
-            print(ex)
-
+        ready_numbers.append(f'{phone_number} - {get_opp(phone_number)}')
         with open(file_name, 'w', encoding='utf-8') as file:
             for item in ready_numbers:
                 file.write(f'{item}\n')
 
 
-if __name__ == '__main__':
-    get_opp('1')
+def get_opp(phone_number) -> str:
+    data = {
+        'number': f'{phone_number}',
+    }
 
+    try:
+        response = requests.post("https://www.kody.su/check-tel", headers=headers,
+                                 data=data, proxies=proxies)
+        html = response.text
+
+        soup = BeautifulSoup(html, "lxml")
+        content_in = soup.find("div", class_="content__in").text.lower()
+        for opp in oppers:
+            if opp in content_in:
+                return f'{opp.capitalize()}'
+        else:
+            return f'Другие'
+    except Exception as ex:
+        print(ex)
+        return
+
+
+if __name__ == '__main__':
+    file_edit('1')
--- tgBot.py
+++ tgBot.py
@@ -2,10 +2,12 @@ import logging
 import os
 
 from config import API_KEY
-from main import get_opp
+from main import get_opp, file_edit
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher import filters
 
 API_TOKEN = API_KEY
+PHONE_REGEXP = r'^\+7|8\D*\d{3}\D*\d{3}\D*\d{2}\D*\d{2}'
 
 logging.basicConfig(level=logging.INFO)
 
@@ -15,7 +17,10 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+    await message.reply("<b>Привет!</b>\n\n"
+                        "Данный бот определяет оператора по номеру телефона.\n\n"
+                        "Для этого напиши боту номер телефона или отправь <i>файл.txt</i> "
+                        "с несколькими номерами телефона!", parse_mode="HTML")
 
 
 @dp.message_handler(content_types=types.ContentType.DOCUMENT)
@@ -26,20 +31,25 @@ async def scan_message(message: types.Message):
         file_name = message.document.file_name
         await message.document.download(destination_file=file_name)
 
-        get_opp(file_name)
+        file_edit(file_name)
 
         await bot.send_document(chat_id=message.from_user.id, document=open(file_name, 'rb'))
 
-        if os.path.isfile(f'temp/{file_name}'):
-            os.remove(f'temp/{file_name}')
+        if os.path.isfile(file_name):
+            os.remove(file_name)
             print("success")
         else:
             print("File doesn't exists!")
 
 
+@dp.message_handler(regexp=PHONE_REGEXP)
+async def regexp_example(message: types.Message):
+    await message.answer(get_opp(message.text))
+
+
 @dp.message_handler()
 async def echo(message: types.Message):
-    await message.answer(message.text)
+    await message.answer('Сообщение не распознано')
 
 
 if __name__ == '__main__':
